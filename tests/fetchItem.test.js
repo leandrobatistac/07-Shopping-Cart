45c4d9362a6aa1a95b074cd3f0b0cb0fe5e57f85
Leandro Batista <leandrobcrv@gmail.com>
2022-10-11 19:54:13 -0300

    Requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,26 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('fetchItem é uma funçao', async () => {
+    expect(typeof fetchItem).toBe('function')
+  });
+
+  it('Verifique se a função fetch foi chamada.', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('Verifique se a função fetch foi chamada com o endpoint correto', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+
+  it('fetchItem sem argumento, retorna um erro com a mensagem', async () => {
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'));
+  });
+
+  it('Retorno da função é uma estrutura de dados igual ao objeto ', async () => {
+    const result = await fetchItem('MLB1615760527');
+    expect(result).toEqual(item);
+  });
 });
