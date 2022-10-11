1149b171db2798067fb49f672f3ee9d80ce2e85a
Leandro Batista <leandrobcrv@gmail.com>
2022-10-11 14:47:41 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Esse tipo de comentário que estão antes de todas as funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições! 
 
-// Fique a vontade para modificar o código já escrito e criar suas próprias funções!
+// Fique a vontade para modificar o código já escrito e criar suas próprias funções! 
 
 /**
  * Função responsável por criar e retornar o elemento de imagem do produto.
