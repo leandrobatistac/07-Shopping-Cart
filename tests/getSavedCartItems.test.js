013084d2c5a80374bc9a1bb5f0d135e0961e8733
Leandro Batista <leandrobcrv@gmail.com>
2022-10-11 20:43:01 -0300

    Requisitos 6 e 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,13 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifique se a função localStorage.setItem foi chamada.', () => {
+    getSavedCartItems('Teste');
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+  });
+
+  it('Verifique se a função localStorage.setItem foi chamada com 2 parametros.', () => {
+    getSavedCartItems('Teste');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItem');
+  });
 });
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,13 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifique se a função localStorage.setItem foi chamada.', () => {
+    saveCartItems('Teste');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+
+  it('Verifique se a função localStorage.setItem foi chamada com 2 parametros.', () => {
+    saveCartItems('Teste');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItem', 'Teste');
+  });
 });
